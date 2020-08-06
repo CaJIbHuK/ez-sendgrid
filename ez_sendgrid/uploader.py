a129005f84d426979b79dc975b023445f56739cd
Anton Zaslavskiy <anton.zaslavskiy@chainstack.com>
2020-08-06 16:33:30 +0300

    Add ability to delete outdated versions by setting `keep` property in the inventory file
    Signed-off-by: Anton Zaslavskiy <anton.zaslavskiy@chainstack.com>

--- ez_sendgrid/uploader.py
+++ ez_sendgrid/uploader.py
@@ -29,6 +29,35 @@ def process_response(resp: Response) -> dict:
     return json.loads(resp.body)
 
 
+def parse_datetime(value: str) -> datetime.datetime:
+    """
+    Parse date.
+
+    '2020-04-23 16:14:45' -> datetime object
+    """
+    return datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+
+
+def delete_old_versions(template_id: str, keep: int, *, sg: sendgrid.SendGridAPIClient) -> None:
+    """
+    Delete old not active versions of template.
+
+    :param template_id: id of sendgrid template
+    :param keep: the number of previous versions to keep (default: 0)
+    :param sg: sendgrid client
+    :return:
+    """
+    template = process_response(sg.client.templates._(template_id).get())
+    versions_to_delete = sorted(
+        filter(lambda v: not v['active'], template['versions']),
+        key=lambda v: parse_datetime(v['updated_at']),
+        reverse=True,
+    )[keep - 1:]
+    for version in versions_to_delete:
+        logging.info(f'Delete version id=`{version["id"]}` template=`{template["id"]}`')
+        sg.client.templates._(template['id']).versions._(version['id']).delete()
+
+
 def processor(inventory_data: list, api_key: str, template_prefix=None) -> None:
     """Run templates sync."""
     sg = sendgrid.SendGridAPIClient(api_key=api_key)
@@ -66,6 +95,11 @@ def processor(inventory_data: list, api_key: str, template_prefix=None) -> None:
             r = sg.client.templates._(template['template_id']).versions._(template['version_id']).patch(request_body=data)
             version_id = process_response(r)['id']
             logging.info(f'Updated version id=`{version_id}` template=`{template_name}`')
+
+        keep = int(template.get('keep', 0))
+        if keep > 0:
+            logging.info(f'Delete old versions of template=`{template_name}`')
+            delete_old_versions(template['template_id'], keep, sg=sg)
 
 
 def inventory_map(inventory_data: list) -> dict:
